9118b513bef1ad53d59a732282b1a55ee49eac59
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-03 12:11:19 -0300

    Resgatando todos os serviços

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -2,22 +2,31 @@ const { PermPhoneMsg } = require("@mui/icons-material");
 const { Service: ServiceModel } = require("../models/Service");
 
 const serviceController = {
-    create: async (req, res) => {
-        try {
-            const service = {
-                name: req.body.name,
-                description: req.body.description,
-                price: req.body.price,
-                image: req.body.image,
-            };
+  create: async (req, res) => {
+    try {
+      const service = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image,
+      };
 
-            const response = await ServiceModel.create(service);
+      const response = await ServiceModel.create(service);
 
-            res.status(201).json({ response, msg: "Serviço criado com sucesso!" });
-        } catch (error) {
-            console.log(error);
-        }
+      res.status(201).json({ response, msg: "Serviço criado com sucesso!" });
+    } catch (error) {
+      console.log(error);
     }
+  },
+  getAll: async (req, res) => {
+    try {
+      const services = await ServiceModel.find();
+
+      res.json(services);
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = serviceController;
