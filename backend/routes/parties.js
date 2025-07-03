640cc42f9c170d7862eb883ca0af894812079d80
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-03 14:47:41 -0300

    Atualizando festas

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -12,4 +12,6 @@ router
   .route("/parties/:id")
   .delete((req, res) => partyController.delete(req, res));
 
+router.route("/parties/:id").put((req, res) => partyController.update(req, res));
+
 module.exports = router;
