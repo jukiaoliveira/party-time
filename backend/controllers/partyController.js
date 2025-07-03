19c83a37bbe9814b95762d40559793bff09c1180
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-03 14:33:25 -0300

    Excluindo festa

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -51,13 +51,26 @@ const partyController = {
 
       if (!party) {
         res.status(404).json({ msg: "Festa não encontrada!" });
-        return
+        return;
       }
 
       res.json(party);
     } catch (error) {
       console.log(error);
     }
+  },
+  delete: async (req, res) => {
+    const id = req.params.id;
+    const party = await PartyModel.findById(id);
+
+    if (!party) {
+      res.status(404).json({ msg: "Festa não encontrada!" });
+      return;
+    }
+
+    const deletedParty = await PartyModel.findByIdAndDelete(id);
+
+    res.status(200).json({ deletedParty, msg: "Festa excluída com sucesso!" });
   },
 };
 
