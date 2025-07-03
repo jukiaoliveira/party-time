a85f7467d33270014500aaee28ef631e31cb27c0
jukiaoliveira <jurafaoliveira@icloud.com>
2025-07-03 14:26:39 -0300

    Encontrando festa individual

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -43,6 +43,22 @@ const partyController = {
       console.log(error);
     }
   },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const party = await PartyModel.findById(id);
+
+      if (!party) {
+        res.status(404).json({ msg: "Festa não encontrada!" });
+        return
+      }
+
+      res.json(party);
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = partyController;
